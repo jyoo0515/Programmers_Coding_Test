4be45f45d0825f369345fc913c7eae0cbe87bc2d
jyoo0515 <jasonyoo0515@gmail.com>
2022-03-24 01:38:35 +0900

    solved signed sum problem

---
+++ signed_sum.cpp
@@ -0,0 +1,23 @@
+// https://programmers.co.kr/learn/courses/30/lessons/76501
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs)
+{
+    int answer = 0;
+    for (int i = 0; i < absolutes.size(); i++)
+    {
+        if (signs[i])
+        {
+            answer += absolutes[i];
+        }
+        else
+        {
+            answer -= absolutes[i];
+        }
+    }
+    return answer;
+}
