06bd94bed17d17fd17006dd47b9f6492ccef0207
jyoo0515 <jasonyoo0515@gmail.com>
2022-05-26 03:32:59 +0900

    java: solved carpet problem

---
+++ java/carpet.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> getDivisors(int num) {
+        ArrayList<Integer> divisors = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                divisors.add(i);
+            }
+        }
+        return divisors;
+    }
+
+    public int[] solution(int brown, int yellow) {
+        ArrayList<Integer> yellow_divisors = getDivisors(yellow);
+        for (int d : yellow_divisors) {
+            int yellow_width = yellow / d;
+
+            if ((2 * yellow_width + 2 * d + 4) == brown) {
+                return new int[]{yellow_width + 2, d + 2};
+            }
+        }
+        return null;
+    }
+}
