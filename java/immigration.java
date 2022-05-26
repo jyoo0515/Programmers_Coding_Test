4934f9ce2a5913b28fbf84e1a0e9138f9444827c
jyoo0515 <jasonyoo0515@gmail.com>
2022-05-26 21:34:32 +0900

    java: solved immigration problem

---
+++ java/immigration.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long max_time = (long) Arrays.stream(times).max().getAsInt() * n;
+        long min_time = 1;
+        long answer = 1;
+
+        while (min_time <= max_time) {
+            long mid = (min_time + max_time) / 2;
+            long people = 0;
+            for (int time: times) {
+                people += mid / time;
+                if (people >= n)
+                    break;
+            }
+            if (people >= n) {
+                answer = mid;
+                max_time = mid - 1;
+            } else {
+                min_time = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+}
