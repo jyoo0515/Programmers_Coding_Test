4c00c3645191a8212f11febd83a258158be246e9
jyoo0515 <jasonyoo0515@gmail.com>
2022-05-26 22:13:06 +0900

    java: solved spicier problem

---
+++ java/spicier.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int s: scoville) {
+            minHeap.add(s);
+        }
+
+        while (minHeap.peek() < K) {
+            if (minHeap.size() == 1) {
+                answer = -1;
+                break;
+            }
+            int min = minHeap.remove();
+            int secondMin = minHeap.remove();
+            int newScov = min + (secondMin * 2);
+            minHeap.add(newScov);
+            answer++;
+        }
+
+        return answer;
+    }
+}
