fb93eea1a7f0de993d20798a03bb4e0b675fb95a
jyoo0515 <jasonyoo0515@gmail.com>
2022-05-26 17:09:09 +0900

    java: solved network problem

---
+++ java/network.java
@@ -0,0 +1,21 @@
+class Solution {
+    void dfs(int[][] computers, boolean[] visited, int cur) {
+        visited[cur] = true;
+        for (int j = 0; j < computers[cur].length; j++) {
+            if (j != cur && !visited[j] && computers[cur][j] == 1)
+                dfs(computers, visited, j);
+        }
+    }
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(computers, visited, i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
