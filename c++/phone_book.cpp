4f50accac3231e10d3a94030c82c2d39209b73eb
jyoo0515 <jasonyoo0515@gmail.com>
2022-06-05 00:59:59 +0900

    cpp: solved phone book problem

---
+++ c++/phone_book.cpp
@@ -0,0 +1,43 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42577
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book)
+{
+    sort(phone_book.begin(), phone_book.end());
+    for (int i = 0; i < phone_book.size(); i++)
+    {
+        if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size()))
+            return false;
+    }
+    return true;
+}
+
+// #include <string>
+// #include <vector>
+// #include <unordered_map>
+
+// using namespace std;
+
+// bool solution(vector<string> phone_book) {
+//     bool answer = true;
+
+//     unordered_map<string, int> hash_map;
+//     for(int i = 0; i < phone_book.size(); i++)
+//         hash_map[phone_book[i]] = 1;
+
+//     for(int i = 0; i < phone_book.size(); i++) {
+//         string phone_number = "";
+//         for(int j = 0; j < phone_book[i].size(); j++) {
+//             phone_number += phone_book[i][j];
+//             if(hash_map[phone_number] && phone_number != phone_book[i])
+//                 answer = false;
+//         }
+//     }
+
+//     return answer;
+// }
