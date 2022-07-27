0002265a46d53519fe07c270a9b69666693cc594
jyoo0515 <jasonyoo0515@gmail.com>
2022-07-27 23:17:29 +0900

    java: solved matrix addition problem

---
+++ java/matrix_addition.java
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                arr1[i][j] += arr2[i][j];
+            }
+        }
+        return arr1;
+    }
+}
